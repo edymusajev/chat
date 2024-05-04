21f704295a40befce65933ebbca91361cf645519
edymusajev <edymusajev@gmail.com>
2024-05-04 10:57:39 +0700

    x

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react";
 
 const socket = new WebSocket("ws://localhost:8080");
 
+// when opening the app the user has 2 options: join a room or create a new room
+// if the user joins a room, the user will be prompted to enter the room id
+// if the user creates a new room, the user will be assigned a room id
+// the user will be able to set how long a room will persist
+// the user will be able to send messages to the room and share the room id with others to join
+// the user will be able to see all the messages sent to the room
+
 function App() {
   const [messages, setMessages] = useState<string[]>([]);
   const [message, setMessage] = useState("");
