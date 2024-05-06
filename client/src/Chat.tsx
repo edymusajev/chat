10d6d80cdf108ebe397d084301ab3c6309b1c1fb
edymusajev <edymusajev@gmail.com>
2024-05-06 12:28:04 +0700

    x

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 const socket = new WebSocket("ws://localhost:8080");
 
+// what we want is to first fetch from the server the chat history with the room id
+const fetchChatHistory = async (roomId: string) => {
+  const res = await fetch(`http://localhost:8080/room/${roomId}`);
+  const data = await res.json();
+  return data.messages;
+};
+// and then we use websockets to send and receive messages in real-time
+
 export const Chat = () => {
   const [messages, setMessages] = useState<string[]>([]);
   const [message, setMessage] = useState("");
@@ -22,6 +30,15 @@ export const Chat = () => {
       setMessages((prevMessages) => messages);
     };
   }, []);
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const roomId = url.searchParams.get("id");
+    if (!roomId) return;
+    fetchChatHistory(roomId).then((messages) => {
+      setMessages(messages);
+    });
+  });
   return (
     <>
       {messages.map((message, index) => (
